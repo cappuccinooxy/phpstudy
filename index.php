e11bb5a69ce1549cd81cbed366e7585a2433ebee
cappuccinooxy <840312201@qq.com>
2016-04-23 15:26:13 +0800

    增加了一个ech

--- index.php
+++ index.php
@@ -167,7 +167,9 @@ sex: <input type="text" name="sex">
 echo '这是第“ ' . __LINE__ . ' ” 行';
 echo '该文件位于“'.__FILE__.'” ';     
 echo '该文件位于“'.__DIR__.'” ';   
+echo "增加";echo "增加";
 echo "增加";
+
 
 ?>
 
